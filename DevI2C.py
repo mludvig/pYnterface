1e4e2140d58ab671c25e6f35413b04cb92946b59
Michal Ludvig <mludvig@logix.net.nz>
2015-08-10 17:30:16 +1200

    Renamed DevI2C.smbus to DevI2C.bus

--- DevI2C.py
+++ DevI2C.py
@@ -12,29 +12,28 @@ __all__ = [ "DevI2C" ]
 class DevI2C(GenericI2C):
     MAX_SEND_BYTES = 32
 
-    def __init__(self, bus):
-        self.bus = bus
-        self.smbus = smbus.SMBus(bus)
+    def __init__(self, i2c_id):
+        self.bus = smbus.SMBus(i2c_id)
 
     def close(self):
-        self.smbus.close()
+        self.bus.close()
 
     def send_bytes(self, address, data = []):
-        self.smbus.write_i2c_block_data(address, data[0], data[1:])
+        self.bus.write_i2c_block_data(address, data[0], data[1:])
 
     def scan_bus(self, min_addr = 0x08, max_addr = 0x77):
         devices = []
         for addr in range(min_addr, max_addr + 1):
             try:
-                self.smbus.read_byte(addr)   # throws IOError if addr is invalid
+                self.bus.read_byte(addr)   # throws IOError if addr is invalid
                 devices.append(addr)
             except IOError as e:
                 pass
         return devices
 
 if __name__ == "__main__":
-    bus = 1
-    i2c = DevI2C(bus)
+    i2c_id = 1  # /dev/i2c-<id>
+    i2c = DevI2C(i2c_id = i2c_id)
     print("Opened bus: i2c-%d" % bus)
     devices = i2c.scan_bus()
     print("Found I2C devices: %s" % (devices and " ".join([hex(ch) for ch in devices]) or "None"))
